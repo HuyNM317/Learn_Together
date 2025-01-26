9c5554997cd7b767b68c24d528ae9a7768cd1fbd
HuyNM317 <huynm@fullstack.edu.vn>
2025-01-26 09:21:21 +0700

    Buoi 2

---
+++ learn_js/functions/js/ex02.js
@@ -0,0 +1,16 @@
+// Declaration function
+
+// function checkNum() {}
+
+// // expression function
+
+// const checkNum = function () {};
+
+// Arrow function
+
+// const checkNum = () => ({ a: 1 });
+// const b = checkNum();
+// console.log(b
+// for (let i = 1; i <= 200; i++) {
+//   fetch("https://api.studyinchina.io/api/school/paginate?page=1&limit=15");
+// }
