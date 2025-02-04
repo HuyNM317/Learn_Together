f5fbc6026388f4d411d65796d0d163444bf49f0d
HuyNM317 <huynm@fullstack.edu.vn>
2025-02-04 22:41:37 +0700

    update code va bai tap buoi 03

--- learn_js/functions/js/ex02.js
+++ learn_js/functions/js/ex02.js
@@ -14,3 +14,17 @@
 // for (let i = 1; i <= 200; i++) {
 //   fetch("https://api.studyinchina.io/api/school/paginate?page=1&limit=15");
 // }
+
+// alert("Alo");
+
+// prompt("Nhập tuổi của bạn:");
+
+// setTimeout(() => {
+//   console.log("ok");
+// }, 1000);
+
+// setInterval(() => {
+//   console.log("ok");
+// }, 1000);
+
+// Built in function
