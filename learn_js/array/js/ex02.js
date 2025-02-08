e0448aa229b7362d5da28c93e75b5b0c0fc0c5c7
HuyNM317 <huynm@fullstack.edu.vn>
2025-02-08 22:27:52 +0700

    Buoi2

---
+++ learn_js/array/js/ex02.js
@@ -0,0 +1,49 @@
+// Các phương thức xử lý mảng
+// console.log(Array.prototype);
+let arr = [1, 2, 3, 4];
+let arr1 = [5, 6, 7];
+
+let str = "Huy";
+
+// console.log(str[0]);
+
+// 1. at(index) => Trả về phần tử theo index
+// console.log(arr.at(2));
+
+// 2. concat(arr1, arr2,....)
+// console.log(arr.concat(arr1));
+
+// 3. indexOf(value) => Tìm vị trí xuất hiện đầu tiên của phần tử cần tìm trong mảng
+// log
+
+// 4. lastIndexOf(value) => .... tìm phần tử cuối cùng trả về index
+
+// 5. includes(value) => Trả về boolean
+
+// 6. slice(start, end) => tương tự chuỗi
+
+// 7. join(sep) => Nối các phần tử trong mảng thành một chuỗi và nó ngắn cách bằng chuỗi được truyền vào
+
+// console.log(arr.join("*"));
+
+// 8. push(value) => Thêm một phần tử vào cuối mảng
+// Trả về số lượng phần tử sau khi thêm
+// Có thay đổi mảng ban đầu
+// console.log(arr.push(6));
+// console.log(arr);
+
+// 9. unshift(value) => Thêm phần tử vào đầu mảng trả về số lượng phần tử của mảng sau khi thêm
+// Có thay đổi mảng gốc
+
+// console.log(arr.unshift(6));
+// console.log(arr);
+
+// 10. pop() => Xóa phần tử cuối mảng và trả về giá trị vừa xóa
+// Có thay đổi mảng gốc
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// 11. shift() => Xóa phần tử đầu mảng trả về phần tử bị xóa
+// Có thay đổi mảng gốc
+// console.log(arr.shift());
